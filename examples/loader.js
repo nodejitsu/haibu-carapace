8b93dc836b5cff120766be080daf4a6131441ba7
Bradley Meck <bradley.meck@gmail.com>
2011-07-15 14:42:42 -0500

    [fix] Connections to carapace should not force it to stay open (this allows long lived connections)

--- examples/loader.js
+++ examples/loader.js
@@ -1,18 +1,20 @@
 var path = require('path');
 var carapace = require('../');
 var bridgePath = path.resolve(process.argv[2]);
+
 console.log('connecting to ' + bridgePath)
 require('dnode').connect(bridgePath, function(client, conn) {
   console.log('loading plugin ' + carapace.plugins.chroot);
+  //
+  // Connections to Carapace do not keep it open!
+  //
+  conn.on('end',function(){
+    console.log('Carapace connection had ended.')
+  });
   client.emit('plugin',carapace.plugins.chroot, function() {
     console.log('plugin done')
-    console.dir(arguments)
     client.emit('chroot:root','..', function() {
       client.emit('run', 'examples/server.js');
-      //
-      // Kill our connection to carapace (this connection will prevent carapace from closing due to IOWatchers)
-      //
-      conn.end();
     });
   })
 });
