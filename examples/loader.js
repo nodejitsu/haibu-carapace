0a933c5d3ce6c1b99bd496b8fdaea3d1ebc325c8
indexzero <charlie.robbins@gmail.com>
2011-07-20 02:34:42 -0700

    [refactor] Breakout `carapace.wrap()` into multiple methods for granularity and create mirrored method / event invocation in plugins/*

--- examples/loader.js
+++ examples/loader.js
@@ -1,32 +1,21 @@
-var carapace = require('../');
+var carapace = require('../lib/carapace');
 var path = require('path');
 var bridge = process.argv.splice(2,1)[0];
 
-carapace.wrap(bridge, function () {
-  carapace.emit('plugin', carapace.plugins.heartbeat, function (err) {
-    if (err) {
-      console.error(err.stack);
-      process.exit();
-    }
-    
-    carapace.emit('plugin', carapace.plugins.chroot, function (err) {
-      if (err) {
-        console.error(err.stack);
-        process.exit();
-      }
-      
-      carapace.emit('plugin', carapace.plugins.chdir, function (err) {
-        if (err) {
-          console.error(err.stack);
-          process.exit();
-        }
-        
-        carapace.emit('chroot:root', './tobechrooted', function (err) {
-          carapace.emit('chdir:path', '.', function (err) {
-            carapace.emit('run', ['./server.js']);
-          });
-        });
-      });
-    });
+carapace.listen(bridge, function () {
+  carapace.on('carapace:plugin:error', function (plugin, ex) {
+    console.log('Error loading plugin: ' + plugin);
+    console.log(ex.message);
+    console.dir(ex.stack.split('\n'))
   });
+  
+  carapace.use([
+    carapace.plugins.heartbeat, 
+    carapace.plugins.chroot, 
+    carapace.plugins.chdir
+  ], function () {
+    carapace.chroot('./tobechrooted');
+    carapace.chdir('.');
+    carapace.run(['./server.js']);
+  });  
 });
