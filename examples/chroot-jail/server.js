935ffbbd4f4bc6d0d40a5dc0a5a99cfa815fb5f0
Jameson Lee <jamesonjlee@gmail.com>
2011-07-26 04:06:53 -0400

    [examples] updated examples, jailer & jailed server

--- examples/chroot-jail/server.js
+++ examples/chroot-jail/server.js
@@ -1,7 +1,16 @@
+var port, server;
 
-var port   = exports.port   = 1337;
-var server = exports.server = require('http').createServer(function (req,res) {
+port   = exports.port   = 1337;
+server = exports.server = require('http').createServer(function (req,res) {
+  console.dir('server.js got a request');
   res.end(process.cwd());
 });
 
+
+// Ultra-Naive port binding
+if (process.argv.length === 4) {
+  port = process.argv[3];
+}
+
+// listen
 server.listen(port);
---
+++ examples/jailer.js
@@ -0,0 +1,32 @@
+var carapace = require('../lib/carapace'),
+    colors = require('colors');
+
+var script = 'server.js',
+    hookOpts = {
+      'debug' : false,
+      'hook-port' :  5061
+    },
+    scriptPort = 31337;
+
+carapace.listen( hookOpts,function () {
+  carapace.on('carapace::plugin::error', function (plugin, ex) {
+    console.log('Error loading plugin: ' + plugin);
+    console.log(ex.message);
+    console.dir(ex.stack.split('\n'))
+  });
+
+  carapace.use([
+    carapace.plugins.heartbeat, 
+    carapace.plugins.chroot, 
+    carapace.plugins.chdir
+  ], function () {
+    carapace.chroot('./examples/chroot-jail', console.log.bind(null, 'hello'));
+    carapace.chdir('.');
+    carapace.run(script, ['--port', scriptPort], function afterRun() {
+      carapace.heartbeat(function () {
+        console.log('bump'.red);
+      },1000);
+      console.log(script.yellow + ' running on port '.grey + scriptPort.toString().green);
+    });
+  });  
+});
