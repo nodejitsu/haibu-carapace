2ab3c37adfc8e2f75a810fe4f90bf0ae5902c332
Bradley Meck <bradley.meck@gmail.com>
2011-08-15 13:47:53 -0500

    [fix] #6 race condition and bound port clobbering thanks @stolsma for reproducable test

---
+++ test/fixtures/multi-server.js
@@ -0,0 +1,49 @@
+/*
+ * Test _doListen function in net.js of haibu.carapace when there are multiple servers
+ *
+ */
+var net = require('net'),
+    port = 8000;
+
+var server1 = net.createServer(function(socket) {
+});
+
+var server2 = net.createServer(function(socket) {
+});
+
+var server3 = net.createServer(function(socket) {
+});
+
+server1.addListener('error', function(err) {
+  process.exit(1);
+});
+
+server2.addListener('error', function(err) {
+  process.exit(2);
+});
+
+server3.addListener('error', function(err) {
+  process.exit(3);
+});
+
+//
+// start server1 to use the test port and address...
+//
+server1.listen(port, 'localhost', function() {
+  var ready = false;
+
+  //
+  // Server1 is occupying the test port and address so now spawn up more servers
+  //
+
+  server2.listen(port, 'localhost', function() {
+    if (ready) process.exit(0);
+    ready = true;
+  });
+
+  server3.listen(port, 'localhost', function() {
+    if (ready) process.exit(0);
+    ready = true;
+  });
+
+});
