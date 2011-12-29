c0b8f0b64af5e15417091f5cfc2c5835233a629d
Maciej Małecki <maciej.malecki@notimplemented.org>
2011-12-29 17:06:44 +0100

    [test] Use new API in `simple/use-load` test

--- test/simple/use-run-test.js
+++ test/simple/use-run-test.js
@@ -11,21 +11,19 @@ var assert = require('assert'),
     helper = require('../helper/macros.js'),
     carapace = require('../../lib/carapace');
 
-var PORT = 5050;
-    
 vows.describe('carapace/simple/use-plugins').addBatch({
-  "When using haibu-carapace":  helper.assertListen(PORT, {
+  "When using haibu-carapace":  {
     "load up chdir, chroot, heartbeat plugins" : helper.assertUse(['chdir', 'chroot', 'heartbeat'], {
       "and running the heartbeat plugin" : {
         topic : function () {
-          carapace.on('carapace::heartbeat', this.callback.bind(carapace, null));
+          carapace.on('heartbeat', this.callback.bind(carapace, null));
           carapace.heartbeat();
         },
-        "should see a carapace::heartbeat event" : function (_, event, data) {
+        "should see a heartbeat event" : function (_, event, data) {
           assert.isString(carapace.event);
-          assert.equal(carapace.event, 'carapace::heartbeat');
+          assert.equal(carapace.event, 'heartbeat');
         }
       }
     })
-  })
+  }
 }).export(module);
